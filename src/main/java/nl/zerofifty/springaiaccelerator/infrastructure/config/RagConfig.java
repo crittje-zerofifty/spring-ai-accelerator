22504cee60ba51be26ab75394268d9e979c0f4e1
ChrisM <christianmartin@zerofifty-it.nl>
2026-03-28 10:42:43 +0100

    basic rag impl

---
+++ src/main/java/nl/zerofifty/springaiaccelerator/infrastructure/config/RagConfig.java
@@ -0,0 +1,18 @@
+package nl.zerofifty.springaiaccelerator.infrastructure.config;
+
+import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Profile("rag")
+@Configuration
+public class RagConfig {
+
+    @Bean
+    public QuestionAnswerAdvisor questionAnswerAdvisor(VectorStore vectorStore) {
+        return QuestionAnswerAdvisor.builder(vectorStore).build();
+    }
+
+}
